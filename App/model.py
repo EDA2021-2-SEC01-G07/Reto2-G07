56e859f7d3c98435fe71cdcd32899889a730f199
TweepCoding <tweepcoding@gmail.com>
2021-10-19 16:52:02 -0500

    Working on req 5

--- App/model.py
+++ App/model.py
@@ -52,11 +52,7 @@ def newCatalog():
     Retorna el catalogo inicializado.
     """
 
-    catalog = {
-        'artists': None,
-        'mediums': None
-        
-    }
+    catalog = {}
 
     """
     Indices creados en el catalogo por medio, nacionalidad, años de nacimiento para autores, id de artistas y obras
@@ -116,7 +112,7 @@ accession_number, classification, department, date_aquired, cataloged, url, circ
 depth, diameter, height, lenght, weight, width, seat_height, duration):
     artwork = {"id": id,
     "title": title,
-    "constituent_id": constituent_id,
+    "constituent_id": constituent_id[1:-1].replace(" ", "").split(","),
     "date": date,
     "medium": medium,
     "dimensions": dimensions,
@@ -183,7 +179,6 @@ def loadNationality(catalog):
     artworks=catalog['artworks']
     for artwork in lt.iterator(artworks):
         code=artwork["constituent_id"] 
-        code=code[1:-1].replace(" ","").split(",")
         
         for artist_id in code:
             artist_nationality=me.getValue(mp.get(artist_map,artist_id))["Nationality"]
@@ -442,6 +437,110 @@ def sortByNationality(catalog):
     
     return sorted_nationalities,unique_artworks, joined
 
+def costFromDepartment(catalog, department):
+
+    returned_values = mp.newMap()
+
+    id_to_artist = catalog["artwork_id"]
+    dept_to_artworks = catalog["department"]
+    
+    total_cost, total_weigth = 0, 0
+    artworks = dept_to_artworks[department]
+
+    for artwork in lt.iterator(artworks):
+        if "cost" not in artwork:
+            artwork["cost"] = getTransportationCost(artwork)
+        total_cost += artwork["cost"]
+        total_weigth += artwork["weigth"]
+        
+    mp.put(returned_values, "artworks", dept_to_artworks)
+    
+    return returned_values
+
+def cmpArtworkByDateAcquiredReversed(artwork1, artwork2)->bool: 
+    """ 
+    Devuelve verdadero (True) si el 'DateAcquired' de artwork1 es menores que el de 
+    artwork2 
+    Args: 
+        artwork1: informacion de la primera obra que incluye su valor 'DateAcquired' 
+        artwork2: informacion de la segunda obra que incluye su valor 'DateAcquired' 
+    """
+    if artwork1["date"]=="Unknown":
+        return artwork2["date"]
+    if artwork2["date"]=="Unknown":
+        return artwork1["date"]
+    return int(artwork1["date"]) < int(artwork2["date"])
+
+def cmpCost(value1, value2):
+    return value1['cost'] > value2['cost']
+
+def getTransportationCost(artwork):
+    cost = 42
+    artworks = ['width', 'height', 'lenght', 'depth']
+    weight = artwork['weight']
+    width = 0
+    height = 0
+    lenght = 0
+    radius = 0
+    dimensions = 0
+    total = 0
+    unit1 = ""
+    unit2 = ""
+
+    for unit in artworks:
+        value = artwork[unit]
+        if value != "0" and value !="Unknown":
+            unit1 = unit
+            width = float(value)
+    if width != 0:
+        dimensions += 1
+    for unit in artworks:
+        value = artwork[unit]
+        if value != "0" and value !="Unknown" and unit != unit1:
+            unit2 = unit
+            height = float(value)
+    if height != 0:
+        dimensions += 1
+    for unit in artworks:
+        value = artwork[unit]
+        if value != "0" and value !="Unknown" and unit not in (unit1, unit2):
+            lenght = float(value)
+            break
+    if lenght != 0:
+        dimensions += 1
+    
+    if artwork['circumference'] != "0" and artwork['circumference']!="Unknown":
+        radius = float(artwork['circumference'])/2*pi
+    elif artwork['diameter'] != "0" and artwork['diameter']!="Unknown":
+        radius = float(artwork['diameter'])/2
+    
+    if radius != 0:
+        dimensions += 2
+    
+    if dimensions == 2 or dimensions ==3:
+        if bool(radius):
+            total = pi * radius**2
+            total/=10000
+            if width != 0:
+                total *= width
+                total/=100
+        else:
+            total = width * height
+            total/=10000
+            if lenght != 0:
+                total *= lenght
+                total/=100
+    
+    if weight != "0" and weight!="Unknown":
+        if total!=0 and total > float(weight):
+            cost = total * 72
+        else:
+            cost = float(weight) *72
+    elif total!=0:
+        cost = total * 72
+    
+    return round(cost,6)
+
 def addArtworkArtists(artworks,artists_map):
     """
     Añade al array artworks los nombres de los artistas para cada obra
@@ -449,7 +548,6 @@ def addArtworkArtists(artworks,artists_map):
     for artwork in lt.iterator(artworks):
         names=[]
         code=artwork["constituent_id"]
-        code=code[1:-1].replace(" ","").split(",")
         for artist_id in code:
             names.append(mp.get(artists_map,artist_id)['value']['DisplayName'])
         artwork["Names"]=names
